39d214e3207b867e399e4e998a1dfa971fe252ea
manqobagwala09-sys <manqobagwala09@gmail.com>
2026-05-10 08:51:47 +0200

    Add Firebase configuration fileAdd Firebase configuration setup for future database integration
    This file contains the Firebase configuration settings for the project.

---
+++ Firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+// Firebase init placeholder (optional upgrade)
+console.log("Firebase config loaded");
