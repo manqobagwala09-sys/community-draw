3275deac35d18b781690c5b4f640a55e700e83bf
manqobagwala09-sys <manqobagwala09@gmail.com>
2026-05-10 08:53:32 +0200

    Add floating WhatsApp support button for user contact

---
+++ whatsapp-button.js
@@ -0,0 +1,12 @@
+let btn = document.createElement("a");
+btn.href = "https://wa.me/27700000000";
+btn.innerText = "WhatsApp Support";
+btn.style.position = "fixed";
+btn.style.bottom = "20px";
+btn.style.right = "20px";
+btn.style.background = "green";
+btn.style.color = "white";
+btn.style.padding = "10px";
+btn.style.textDecoration = "none";
+
+document.body.appendChild(btn);
